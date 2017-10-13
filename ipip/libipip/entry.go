7c59e2829493ab82365b4dbf517dae34a6b3bd2a
ljf10000 <l_j_f@163.com>
2017-10-13 10:13:40 +0800

    .

--- ipip/libipip/entry.go
+++ ipip/libipip/entry.go
@@ -14,33 +14,33 @@ type IpEntry struct {
 	City            SymbolDesc // 地区或城市 （国内）
 	Organization    SymbolDesc // 学校或单位 （国内）
 	Network         SymbolDesc // 运营商
-	Lng             SymbolDesc // 维度
-	Lat             SymbolDesc // 经度
+	Lat             SymbolDesc // 纬度
+	Lng             SymbolDesc // 经度
 	TimeZone        SymbolDesc // 时区
 	UTC             SymbolDesc // UTC
-	RegionalismCode [6]byte      // 中国行政区划代码
-	PhoneCode       [4]byte      // 国际电话代码
-	CountryCode     [2]byte      // 国家二位代码
-	ContinentCode   [2]byte      // 世界大洲代码 [AF]非洲, [EU]欧洲, [AS]亚洲, [OA]大洋洲, [NA]北美洲, [SA]南美洲, [AN]南极洲
+	RegionalismCode [6]byte    // 中国行政区划代码
+	PhoneCode       [4]byte    // 国际电话代码
+	CountryCode     [2]byte    // 国家二位代码
+	ContinentCode   [2]byte    // 世界大洲代码 [AF]非洲, [EU]欧洲, [AS]亚洲, [OA]大洋洲, [NA]北美洲, [SA]南美洲, [AN]南极洲
 }
 */
 
 const (
 	FieldIpMin           = 0
 	FieldIpMax           = 1
-	FieldCountry         = 2
-	FieldProvince        = 3
-	FieldCity            = 4
-	FieldOrganization    = 5
-	FieldNetwork         = 6
-	FieldLng             = 7
-	FieldLat             = 8
-	FieldTimeZone        = 9
-	FieldUTC             = 10
-	FieldRegionalismCode = 11
-	FieldPhoneCode       = 12
-	FieldCountryCode     = 13
-	FieldContinentCode   = 14
+	FieldCountry         = 2  // 国家
+	FieldProvince        = 3  // 省会或直辖市（国内）
+	FieldCity            = 4  // 地区或城市 （国内）
+	FieldOrganization    = 5  // 学校或单位 （国内）
+	FieldNetwork         = 6  // 运营商
+	FieldLat             = 7  // 纬度
+	FieldLng             = 8  // 经度
+	FieldTimeZone        = 9  // 时区
+	FieldUTC             = 10 // UTC
+	FieldRegionalismCode = 11 // 中国行政区划代码
+	FieldPhoneCode       = 12 // 国际电话代码
+	FieldCountryCode     = 13 // 国家二位代码
+	FieldContinentCode   = 14 // 世界大洲代码
 	FieldEnd             = 15
 )
 
@@ -84,13 +84,13 @@ var fieldInfos = [FieldEnd]fieldInfo{
 		offset: 16,
 		size:   4,
 	},
-	FieldLng: fieldInfo{
-		name:   "Lng",
+	FieldLat: fieldInfo{
+		name:   "Lat",
 		offset: 20,
 		size:   4,
 	},
-	FieldLat: fieldInfo{
-		name:   "Lat",
+	FieldLng: fieldInfo{
+		name:   "Lng",
 		offset: 24,
 		size:   4,
 	},
